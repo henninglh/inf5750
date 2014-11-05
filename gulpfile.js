61edee4bdd7db66c88f4221f5d668c631eaa89ff
Kjetil Sletten <kjsletten@gmail.com>
2014-11-05 18:16:27 +0100

    Final structure for the environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,26 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jade = require('gulp-jade');
+var minifyCSS = require('gulp-minify-css');
+var karma = require('gulp-karma');
+var zip = require('gulp-zip');
 
-var baseDir = './app/';
+var baseDir = './dist/';
 var scriptDir = 'js/';
-var styleDir = 'styles/'
+var styleDir = 'css/';
 
 var testFiles = [
-    'test/client/*.js'
+    'test/test-main-js'
 ];
 
 gulp.task('server', function() {
     connect.server({
-        root: baseDir,
+        root: 'dist',
         port: 3000,
         livereload: true
     });
-});
-
-gulp.task('scripts', function() {
-    gulp.src(scriptDir)
-        .pipe(connect.reload());
-});
-
-gulp.task('styles', function(){
-   gulp.src(styleDir)
-       .pipe(connect.reload());
 });
 
 gulp.task('test', function() {
@@ -39,7 +35,67 @@ gulp.task('test', function() {
         });
 });
 
-gulp.task('build', function() {
+gulp.task('watch', function(){
+    gulp.watch('./src/css/*.css', ['styles']);
+    gulp.watch('./src/js/**/*.js', ['scripts']);
+    gulp.watch('./src/**/*.jade', ['templates']);
 
 });
+
+gulp.task('default', ['build', 'server', 'watch']);
+
+gulp.task('build', ['scripts', 'templates', 'styles', 'load']);
+
+gulp.task('templates', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./src/views/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./dist/views/'))
+        .pipe(connect.reload())
+
+    gulp.src('./src/index.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(connect.reload())
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(connect.reload())
+});
+
+gulp.task('styles', function() {
+    return  gulp.src('./src/css/*.css')
+        .pipe(concat('style.min.css'))
+        .pipe(minifyCSS({keepBreaks: true}))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(connect.reload())
+});
+
+gulp.task('load', function(){
+    gulp.src('./bower_components/**/*.min.js*')
+        .pipe(gulp.dest('./dist/js/'))
+    gulp.src('./bower_components/**/*.min.css')
+        .pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('deploy', function(){
+    return gulp.src('dist/*')
+        .pipe(zip('ArchitectWirelessWebServices.zip'))
+        .pipe(gulp.dest('dist'))
+});
+
+
+
+
+
+
 
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+var app = angular.module('dataElementBulkEditorApp', []);
+
+console.log('app.js loaded');
