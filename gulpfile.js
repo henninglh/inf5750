c3f9a2efa275c48c22c464a403450ab643304967
Stian Sandvold <stian@asio.no>
2014-11-06 13:41:44 +0100

    Fixed the problem with livereload not updating CSS. The problem occured in the index.jade file, where the rel tag was set as styleSheet and not stylesheet (all lowercase).
    Also commented the gulpfile, incase group-teachers wants\needs to look at it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var jade = require('gulp-jade');
-var minifyCSS = require('gulp-minify-css');
-var karma = require('gulp-karma');
-var zip = require('gulp-zip');
-
-var baseDir = './dist/';
-var scriptDir = 'js/';
-var styleDir = 'css/';
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    jade = require('gulp-jade'),
+    minifyCSS = require('gulp-minify-css'),
+    karma = require('gulp-karma'),
+    zip = require('gulp-zip');
 
 var testFiles = [
     'test/test-main-js'
 ];
 
-gulp.task('server', function() {
+/**
+ * Our default task, starts the build server, to make sure all
+ * required files are present, then starts the server and watch
+ * tasks for development
+ **/
+gulp.task('default', ['build', 'server', 'watch']);
+
+/**
+ * Builds our source files into minified, rendered files,
+ * ready for production
+ **/
+gulp.task('build', ['scripts', 'templates', 'styles', 'load']);
+
+/**
+ * Creates a web server for serving our files @ localhost:3000
+ **/
+gulp.task('server', function () {
     connect.server({
         root: 'dist',
         port: 3000,
@@ -23,30 +35,21 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('test', function() {
-    return gulp.src(testFiles)
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'run'
-        }))
-        .on('error', function(err){
-            console.log(err);
-            this.emit('end');
-        });
-});
-
-gulp.task('watch', function(){
+/**
+ * Watches our source files, and starts processing if any
+ * changes occurs
+ **/
+gulp.task('watch', function () {
     gulp.watch('./src/css/*.css', ['styles']);
     gulp.watch('./src/js/**/*.js', ['scripts']);
     gulp.watch('./src/**/*.jade', ['templates']);
-
 });
 
-gulp.task('default', ['build', 'server', 'watch']);
-
-gulp.task('build', ['scripts', 'templates', 'styles', 'load']);
-
-gulp.task('templates', function() {
+/**
+ * Templates takes our source's .jade files and renders them in HTML
+ * in the /dist& folder, ready for produciton
+ **/
+gulp.task('templates', function () {
     var YOUR_LOCALS = {};
 
     gulp.src('./src/views/*.jade')
@@ -54,45 +57,76 @@ gulp.task('templates', function() {
             locals: YOUR_LOCALS
         }))
         .pipe(gulp.dest('./dist/views/'))
-        .pipe(connect.reload())
+        .pipe(connect.reload());
 
     gulp.src('./src/index.jade')
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
         .pipe(gulp.dest('./dist/'))
-        .pipe(connect.reload())
+        .pipe(connect.reload());
 });
 
-gulp.task('scripts', function() {
+/**
+ * Scripts takes our source's .js files and uglifies them, and concatenate
+ * them into a single app.min.js file, ready for production
+ **/
+gulp.task('scripts', function () {
     return gulp.src('./src/js/**/*.js')
         .pipe(uglify())
         .pipe(concat('app.min.js'))
         .pipe(gulp.dest('dist/js/'))
-        .pipe(connect.reload())
+        .pipe(connect.reload());
 });
 
-gulp.task('styles', function() {
-    return  gulp.src('./src/css/*.css')
+/**
+ * Styles takes our source's .css files, concatenate them into a single file
+ * before minifying it, making t ready for production.
+ **/
+gulp.task('styles', function () {
+    return gulp.src('./src/css/*.css')
         .pipe(concat('style.min.css'))
         .pipe(minifyCSS({keepBreaks: true}))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(connect.reload())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(connect.reload());
 });
 
-gulp.task('load', function(){
+/**
+ * Load takes all our external dependencies, and add the minified versions
+ * to our dist/ folder, making the deployed version independant of external
+ * servers for these libraries(cdn)
+ **/
+gulp.task('load', function () {
     gulp.src('./bower_components/**/*.min.js*')
-        .pipe(gulp.dest('./dist/js/'))
+        .pipe(gulp.dest('./dist/js/'));
     gulp.src('./bower_components/**/*.min.css')
-        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('deploy', function(){
+/**
+ * Deploy makes the entire /dist/ folder into a zipped file, making it a complete,
+ * reaady to install, DHIS 2 application.
+ */
+gulp.task('deploy', function () {
     return gulp.src('dist/*')
         .pipe(zip('ArchitectWirelessWebServices.zip'))
         .pipe(gulp.dest('dist'))
 });
 
+/**
+ * Runs our tests with Karma test-runner
+ **/
+gulp.task('test', function () {
+    return gulp.src(testFiles)
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', function (err) {
+            console.log(err);
+            this.emit('end');
+        });
+});
 
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,1 @@
 var app = angular.module('dataElementBulkEditorApp', []);
-
-console.log('app.js loaded');
