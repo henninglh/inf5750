c2f0039fe744f87fa10435ad3b3cb7ef75a1fae8
VegarM <vegar.molvig@gmail.com>
2014-11-11 20:38:03 +0100

    Fixed small list wrapping (not good yet, but better.)

--- src/js/controllers/listCtrl.js
+++ src/js/controllers/listCtrl.js
@@ -14,55 +14,10 @@ app.controller("ListController", ['$scope', '$location', 'dataElementService', f
         }
     );
 
-    this.currentElement = -1;
-    this.editItem = -1;
     $scope.dataElements = [];
     dataElementService.getAllElements().then(function(data) {
         $scope.dataElements = data.dataElements;
     });
-    /*
-        [
-        {
-            name: "Element one",
-            id: 0
-        },
-        {
-            name: "Element two",
-            id: 1
-        },
-        {
-            name: "Element three",
-            id: 2
-        },
-        {
-            name: "Element four",
-            id: 3
-        },
-        {
-            name: "Element five",
-            id: 4
-        },
-        {
-            name: "Element six",
-            id: 5
-        },
-        {
-            name: "Element seven",
-            id: 6
-        },
-        {
-            name: "Element eight",
-            id: 7
-        },
-        {
-            name: "Element nine",
-            id: 8
-        },
-        {
-            name: "Element ten",
-            id: 9
-        }
-    ];*/
 
     $scope.selectElement = function(element) {
         for(var i = 0; i < $scope.dataElements.length; i++) {
